19b2e92a936002f01654a7bbf35af88fdc4893f1
Jan Hutar <jhutar@redhat.com>
2024-12-16 21:03:50 +0100

    feat: Learn how to send notification

---
+++ meetings_notifier.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+
+import gi
+
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk
+
+gi.require_version("Gio", "2.0")
+from gi.repository import Gio
+
+import playsound
+
+
+class MyWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_default_size(300, 200)
+
+        # Create a button
+        button = Gtk.Button.new_with_label("Send Notification")
+        button.connect("clicked", self.on_button_clicked)
+
+        # Create a vertical box container
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        vbox.append(button)
+
+        self.set_child(vbox)
+
+    def on_button_clicked(self, button):
+        # Create a notification
+        notification = Gio.Notification.new("Hello!")
+        notification.set_body("Hello from GTK4!")
+        notification.set_priority(Gio.NotificationPriority.NORMAL)
+
+        # Send the notification
+        self.get_application().send_notification("notification", notification)
+
+        # Play a sound
+        playsound.playsound("/usr/share/sounds/alsa/Front_Center.wav")
+
+class MyApp(Gtk.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def do_activate(self):
+        window = MyWindow(application=self)
+        window.present()
+
+if __name__ == "__main__":
+    app = MyApp(application_id="com.github.jhutar.meetings_notifier")
+    app.run()
