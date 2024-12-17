487d4046cec8c7bd91ce9bf46b4441e4d14c3114
Jan Hutar <jhutar@redhat.com>
2024-12-17 11:11:38 +0100

    refactor: Do some cleanup

--- gtk-example.py
+++ gtk-example.py
@@ -1,64 +1,40 @@
 #!/usr/bin/env python3
 
-# gtk-example.py
-# (c) Aleksander Alekseev 2016
-# http://eax.me/
+# Original code (C) Aleksander Alekseev 2016, http://eax.me/
 
 import signal
 import os
 import gi
-gi.require_version('Gtk', '3.0')
-gi.require_version('Notify', '0.7')
+
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+
+gi.require_version("Notify", "0.7")
 from gi.repository import Notify
 
-APPID = "GTK Test"
-CURRDIR = os.path.dirname(os.path.abspath(__file__))
-# could be PNG or SVG as well
-ICON = os.path.join(CURRDIR, 'python3.xpm')
 
-# Cross-platform tray icon implementation
-# See:
-# * http://ubuntuforums.org/showthread.php?t=1923373#post11902222
-# * https://github.com/syncthing/syncthing-gtk/blob/master/syncthing_gtk/statusicon.py
-class TrayIcon:
+APPID = "com.github.jhutar.meetings-notifier"
+CURRDIR = os.path.dirname(os.path.abspath(__file__))
+ICON = os.path.join(CURRDIR, "python3.xpm")
+
+
+class MyStatusIcon:
 
     def __init__(self, appid, icon, menu):
         self.menu = menu
 
-        APPIND_SUPPORT = 1
-        try:
-            from gi.repository import AppIndicator3
-        except:
-            APPIND_SUPPORT = 0
-
-        if APPIND_SUPPORT == 1:
-            self.ind = AppIndicator3.Indicator.new(
-                appid, icon,
-                AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
-            self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-            self.ind.set_menu(self.menu)
-        else:
-            self.ind = Gtk.StatusIcon()
-            self.ind.set_from_file(icon)
-            self.ind.connect('popup-menu', self.onPopupMenu)
+        self.ind = Gtk.StatusIcon()
+        self.ind.set_from_file(icon)
+        self.ind.connect("popup-menu", self.onPopupMenu)
 
     def onPopupMenu(self, icon, button, time):
-        self.menu.popup(None, None, Gtk.StatusIcon.position_menu, icon,
-                        button, time)
+        self.menu.popup(None, None, Gtk.StatusIcon.position_menu, icon, button, time)
 
 
-class Handler:
+class MyHandler:
 
     def __init__(self):
-        self.window_is_hidden = False
-
-    def onShowButtonClicked(self, button):
-        msg = "Clicked: " + entry.get_text()
-        dialog = Gtk.MessageDialog(window, 0, Gtk.MessageType.INFO,
-                                   Gtk.ButtonsType.OK, msg)
-        dialog.run()
-        dialog.destroy()
+        self.window_is_hidden = True
 
     def onNotify(self, *args):
         Notify.Notification.new("Notification", "Hello!", ICON).show()
@@ -75,20 +51,24 @@ class Handler:
         Notify.uninit()
         Gtk.main_quit()
 
+
 # Handle pressing Ctr+C properly, ignored by default
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+handler = MyHandler()
+
 builder = Gtk.Builder()
-builder.add_from_file('gtk-example.glade')
-builder.connect_signals(Handler())
+builder.add_from_file("gtk-example.glade")
+builder.connect_signals(handler)
 
-window = builder.get_object('window1')
+window = builder.get_object("window1")
 window.set_icon_from_file(ICON)
-window.show_all()
+window.hide()
+handler.window_is_hidden = True
 
-entry = builder.get_object('entry1')
-menu = builder.get_object('menu1')
-icon = TrayIcon(APPID, ICON, menu)
+menu = builder.get_object("menu1")
+icon = MyStatusIcon(APPID, ICON, menu)
+
 Notify.init(APPID)
 
 Gtk.main()
