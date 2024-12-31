033b398417c6a6e3367bb1d69999cb496ee6de62
Jan Hutar <jhutar@redhat.com>
2024-12-31 07:49:41 +0100

    feat: Hiding window and updating content of the window

--- meetings_notifier/my_ui.py
+++ meetings_notifier/my_ui.py
@@ -9,6 +9,8 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+from gi.repository import GObject
+
 gi.require_version("Notify", "0.7")
 from gi.repository import Notify
 
@@ -21,40 +23,49 @@ CURRDIR = os.path.dirname(os.path.abspath(__file__))
 ICON = os.path.join(CURRDIR, "python3.xpm")
 
 
-class MyStatusIcon:
+class MyHandler:
 
-    def __init__(self, appid, icon, menu):
-        self.menu = menu
+    def __init__(self, builder, calendar):
+        self.calendar = calendar
 
-        self.ind = Gtk.StatusIcon()
-        self.ind.set_from_file(icon)
-        self.ind.connect("popup-menu", self.onPopupMenu)
+        self.builder = builder
+        self.builder.connect_signals(self)
+
+        self.window = self.builder.get_object("window1")
+        self.window.set_icon_from_file(ICON)
+        self.window.hide()
+        self.window_is_hidden = True
+
+        self.menu = builder.get_object("menu1")
+        self.trayicon = Gtk.StatusIcon()
+        self.trayicon.set_from_file(ICON)
+        self.trayicon.connect("popup-menu", self.onPopupMenu)
+        self.trayicon.connect("activate", self.onShowOrHide)
+
+        text_view = self.builder.get_object("text1")
+        self.buffer = text_view.get_buffer()
+
+        self.onTextChange()
 
     def onPopupMenu(self, icon, button, time):
         self.menu.popup(None, None, Gtk.StatusIcon.position_menu, icon, button, time)
 
-
-class MyHandler:
-
-    def __init__(self, calendar):
-        self.calendar = calendar
-
-        self.window = None
-        self.window_is_hidden = True
-
     def onNotify(self, *args):
         Notify.Notification.new("Notification", str(self.calendar.get_closest_meeting()), ICON).show()
-
-        # Play a sound
         my_sound.play()
 
     def onShowOrHide(self, *args):
         if self.window_is_hidden:
             self.window.show()
+            self.window_is_hidden = False
         else:
             self.window.hide()
+            self.window_is_hidden = True
 
-        self.window_is_hidden = not self.window_is_hidden
+    def onTextChange(self):
+        text = str(self.calendar.get_closest_meeting())
+        self.buffer.set_text(text)
+        GObject.timeout_add_seconds(3, self.onTextChange) 
 
     def onQuit(self, *args):
         Notify.uninit()
@@ -66,28 +77,11 @@ def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     calendar = my_calendar.MyCalendar()
-    ###print(calendar.get_closest_meeting())
-    ###import sys
-    ###sys.exit()
-
-    handler = MyHandler(calendar)
 
     builder = Gtk.Builder()
     builder.add_from_file(os.path.join(CURRDIR, "meetings_notifier.glade"))
-    builder.connect_signals(handler)
 
-    window = builder.get_object("window1")
-    window.set_icon_from_file(ICON)
-    window.hide()
-    handler.window = window
-    handler.window_is_hidden = True
-
-    text_view = builder.get_object("text1")
-    buffer = text_view.get_buffer()
-    buffer.set_text(str(calendar.get_closest_meeting()))
-
-    menu = builder.get_object("menu1")
-    icon = MyStatusIcon(APPID, ICON, menu)
+    handler = MyHandler(builder, calendar)
 
     print(calendar.get_closest_meeting())
 
