134db096e6598bc0e12f80d0bd01409f40a40959
Jan Hutar <jhutar@redhat.com>
2025-01-02 13:49:59 +0100

    feat: Add first version of actual alerts

--- meetings_notifier/my_ui.py
+++ meetings_notifier/my_ui.py
@@ -2,6 +2,8 @@
 
 # Original code (C) Aleksander Alekseev 2016, http://eax.me/
 
+import datetime
+import logging
 import signal
 import os
 import gi
@@ -21,6 +23,14 @@ from . import my_sound
 APPID = "com.github.jhutar.meetings-notifier"
 CURRDIR = os.path.dirname(os.path.abspath(__file__))
 ICON = os.path.join(CURRDIR, "python3.xpm")
+TIMER_CALENDAR_REFRESH = 60
+TIMER_WINDOW_TEXT_REFRESH = 3
+TIMER_ALERT_CHECK = 3
+ALERT_URGENCY_1_AFTER = 100
+ALERT_URGENCY_1_AFTER = (datetime.datetime.now(datetime.timezone.utc) - datetime.datetime.fromisoformat("2025-01-01T16:00:00+00:00")).seconds + 10
+ALERT_URGENCY_2_AFTER = 30
+ALERT_URGENCY_3_AFTER = 10
+ALERT_IGNORE_AFTER = -600
 
 
 def event_to_text(event):
@@ -30,11 +40,18 @@ def event_to_text(event):
         return f"When: {event['start']['dateTime'].isoformat()}\nWhat: {event['summary']}\n"
 
 
+def event_to_log(event):
+    if event == {}:
+        return "No event"
+    else:
+        return f"{event['summary']}/{event['id']}({event['start']['dateTime'].isoformat()})"
+
+
 class MyHandler:
 
     def __init__(self, builder, calendar):
         self.calendar = calendar
-        GObject.timeout_add_seconds(60, self.calendar.refresh_events)
+        GObject.timeout_add_seconds(TIMER_CALENDAR_REFRESH, self.calendar.refresh_events)
 
         self.builder = builder
         self.builder.connect_signals(self)
@@ -52,9 +69,10 @@ class MyHandler:
 
         text_view = self.builder.get_object("text1")
         self.buffer = text_view.get_buffer()
-        GObject.timeout_add_seconds(3, self.onTextChange)
-
+        GObject.timeout_add_seconds(TIMER_WINDOW_TEXT_REFRESH, self.onTextChange)
         self.onTextChange()
+
+        GObject.timeout_add_seconds(TIMER_WINDOW_TEXT_REFRESH, self.onAlertCheck)
 
     def onPopupMenu(self, icon, button, time):
         self.menu.popup(None, None, Gtk.StatusIcon.position_menu, icon, button, time)
@@ -83,6 +101,27 @@ class MyHandler:
     def onQuit(self, *args):
         Notify.uninit()
         Gtk.main_quit()
+
+    def onAlertCheck(self):
+        event = self.calendar.get_closest_meeting()
+        if event == {}:
+            return
+
+        now = datetime.datetime.now(datetime.timezone.utc)
+        event_in = (event["start"]["dateTime"] - now).seconds
+        if event_in < ALERT_IGNORE_AFTER:
+            logging.warning(f"Ignoring event {event_to_log(event)} as it is overdue: {event_in}")
+        elif event_in < ALERT_URGENCY_3_AFTER:
+            logging.info(f"Event {event_to_log(event)} almost starts: {event_in}")
+        elif event_in < ALERT_URGENCY_2_AFTER:
+            logging.info(f"Event {event_to_log(event)} starts in a bit: {event_in}")
+        elif event_in < ALERT_URGENCY_1_AFTER:
+            logging.info(f"Event {event_to_log(event)} soon to start: {event_in}")
+            self.onNotify()
+        else:
+            pass
+
+        return True
 
 
 def main():
